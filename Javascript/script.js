3095b3fc5a373a377942ceb2d6386c3dfa7a7617
David Bandana Jusu <davidjusu218@gmail.com>
2023-06-29 22:46:35 +0000

    Initial Commit

---
+++ Javascript/script.js
@@ -0,0 +1,33 @@
+// script.js
+function changeText() {
+    var paragraph = document.querySelector('p');
+    paragraph.textContent = 'Button clicked!';
+}
+
+// script.js
+
+// Gallery section functionality
+function shuffleGallery() {
+    const galleryGrid = document.getElementById('gallery-grid');
+    const gridItems = Array.from(galleryGrid.getElementsByClassName('grid-item'));
+    gridItems.forEach(item => {
+        item.style.order = Math.floor(Math.random() * gridItems.length).toString();
+    });
+}
+
+// Projects section functionality
+function showDetails(projectId) {
+    const projectDetails = document.getElementById('project-details');
+
+    // Clear previous details
+    projectDetails.innerHTML = '';
+
+    // Create and populate the project details
+    const details = document.createElement('div');
+    details.innerHTML = `
+        <h4>Details of ${projectId}</h4>
+        <p>Additional details about the project ${projectId} can be displayed here.</p>
+    `;
+
+    projectDetails.appendChild(details);
+}
